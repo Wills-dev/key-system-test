675e48474cf55095ed507b30fef6e9c60e546b3d
wills-dev <ejioguvictor6@gmail.com>
2025-04-06 12:43:04 +0100

    feat: folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import HomePage from "../src/pages/HomePage/HomePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />{" "}
+    </>
+  );
+}
+
+export default App;
